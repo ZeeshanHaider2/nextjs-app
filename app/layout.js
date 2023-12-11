0c5955afb47d1da4924f103455afb9cc52f0dc06
Zeeshan Haider <39421284+ZeeshanHaider2@users.noreply.github.com>
2023-12-11 23:42:12 +0100

    Navbar added to children

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <nav className='text-2xl text-primary'>This is Navbar</nav>
+        {children}</body>
     </html>
   )
 }
