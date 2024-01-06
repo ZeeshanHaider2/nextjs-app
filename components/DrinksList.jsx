3bc1e3e3434a4bd010cfa576fe763e274b4b77a4
Zeeshan Haider <39421284+ZeeshanHaider2@users.noreply.github.com>
2024-01-06 20:43:02 +0100

    dynamic fetching

---
+++ components/DrinksList.jsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+const DrinksList = ({drinks}) => {
+  return (
+    <ul className="menu menu-vertical pl-0">
+        {drinks.map((drink)=>{
+            return <li key={drink.idDrink}className="text-xl font-medium">
+                <Link href={`/drinks/${drink.idDrink}`}>
+                    {drink.strDrink}
+                </Link>
+            </li>
+        })}
+    </ul>
+  )
+}
+
+export default DrinksList
