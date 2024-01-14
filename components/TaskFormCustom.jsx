d4ef979f529c5f2a39bf381140ae5ca54b4e6b69
Zeeshan Haider <39421284+ZeeshanHaider2@users.noreply.github.com>
2024-01-14 23:18:01 +0100

    error handling

--- components/TaskFormCustom.jsx
+++ components/TaskFormCustom.jsx
@@ -1,6 +1,6 @@
 'use client';
 import { createTaskCustom } from "@/utils/actions"; 
-import { useFormStatus} from "react-dom";
+import { useFormStatus, useFormState} from "react-dom";
 
 const SubmitBtn = () =>{
     const {pending} = useFormStatus()
@@ -12,9 +12,17 @@ const SubmitBtn = () =>{
                  </button>
     )
 }
+
+const initialState ={
+   message: null,
+};
+
+
 const TaskForm = () => {
+    const [state, formAction ] = useFormState(createTaskCustom, initialState);
   return (
-    <form action={createTaskCustom}>
+    <form action={formAction}>
+     {state.message ? <p className='mb-2'>{state.message}</p> :null}
     <div className="join w-full">
         <input
           type='text'
